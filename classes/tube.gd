652a8ed5bb012213138407ec3a44ff46b9aeabf3
Morfy <morfyy1313@gmail.com>
2026-05-14 19:27:25 +0200

    feat: setup basic painting scene

--- classes/tube.gd
+++ classes/tube.gd
@@ -5,6 +5,8 @@ class_name Tube
 @export var color:Color = Color.RED
 @export var target:Paint = null
 
+func _ready() -> void:
+	connect("pressed",_on_pressed)
 
 func _on_pressed() -> void:
 	if target == null:
