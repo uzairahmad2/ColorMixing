b9eb92681b5ab0409c66fcc0aaa2527941a9199c
Morfy <morfyy1313@gmail.com>
2026-05-14 16:48:43 +0200

    feat: implement simple mixing scene (+addon Mixbox)

--- classes/paint.gd
+++ classes/paint.gd
@@ -1,6 +1,7 @@
 extends ColorRect
 class_name Paint
 
+var Mixbox = preload("res://addons/mixbox/mixbox.gd")
 
 var empty:bool = true
 
@@ -9,12 +10,12 @@ func _process(_delta:float) -> void:
 	visible = !empty
 
 
-func add(m_color:Color) -> void:
+func add(color2:Color, amount:float=0.07) -> void:
 	if empty:
-		color = m_color
+		color = color2
 		empty = false
 	else:
-		color += m_color
+		color = Mixbox.lerp(color,color2,amount)
 
 
 func clear() -> void:
